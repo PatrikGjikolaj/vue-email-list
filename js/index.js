65f4a74062775efdd67187987f4e3c1f94127cf2
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-05-17 17:43:39 +0200

    pulito il codice

--- js/index.js
+++ js/index.js
@@ -6,16 +6,13 @@ createApp({
 
             mails: [],
             booleanMails: 'https://flynn.boolean.careers/exercises/api/random/mail',
-            risposta: null
         }
     },
     mounted() {
     
-
         for (let i = 0; i < 10; i++) {   
         axios.get( this.booleanMails ).then((risposta) => {
-                this.risposta = risposta.data.response;
                 this.mails.push(risposta.data.response);
         });
-    }}
+    }},
 }).mount('#app')
